f2a9a4696a524331dca93d8eceba4daf60dc5a91
MonemouZator <monemouzator@gmail.com>
2025-12-06 18:47:10 +0000

    Add python

--- novaweb/settings.py
+++ novaweb/settings.py
@@ -134,3 +134,6 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
 }
+
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
